0492409bbec09629218085a03faf4cdf129f5dca
rduraj96 <durajrei@gmail.com>
2024-01-24 01:44:17 -0500

    Added link to nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ModeToggle } from "./togglleButton";
+import Link from "next/link";
 
 type Props = {};
 
@@ -7,14 +8,14 @@ const Navbar = (props: Props) => {
   return (
     <div className="p-12 pb-0 w-full flex justify-between items-center">
       {/* <Image src={"/logo.svg"} alt="logo" height={60} width={60} /> */}
-      <div className="flex cursor-pointer">
+      <Link href={"/plan"} className="flex cursor-pointer">
         <span className="text-primary text-5xl font-extrabold hover:animate-spin">
           +
         </span>
         <span className="text-6xl font-black text-black dark:text-white">
           R
         </span>
-      </div>
+      </Link>
       <ModeToggle />
     </div>
   );
